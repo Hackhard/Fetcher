a785c2ace50645635bd30ad89e39faf20c64bf5c
Hackhard <abishekhmjee@gmail.com>
2021-04-20 15:42:44 +0530

    Adding data related to Error websites

--- exp.py
+++ exp.py
@@ -1,12 +1,16 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
-import json,time, pickle, pathlib,os
+import json
+import time
+import pickle
+import pathlib
+import os
 
 
-# url = "https://check.torproject.org/"
-# url = "https://dan.me.uk/"
+# url = "check.torproject.org/"
+# url = "dan.me.uk/"
 
-def open_non_tor(url, webs,path):
+def open_non_tor(url, webs, path):
     cook = {}
     ### OPEN USING TOR CONFIG ###
     print("Opening Non-Tor...")
@@ -19,20 +23,21 @@ def open_non_tor(url, webs,path):
 
     print("---------------HTML(NON-TOR)-----------------")
     print()  # BLANK LINE
+    time.sleep(20)
     all_text = browser.page_source
     soup = BeautifulSoup(all_text, 'html.parser')
 
-    #BS4 prettify 
+    # BS4 prettify
 
-    print("Length   ",len(all_text)) # Length
+    print("Length   ", len(all_text))  # Length
     print("All Nodes (Tor): ")
     # Number of nodes
-    i=0
+    i = 0
     for tag in soup.find_all(True):
-        i+=1
-        print(i," -- ", tag.name) # Print the naemes of DOM Nodes
+        i += 1
+        print(i, " -- ", tag.name)  # Print the naemes of DOM Nodes
     print()
-    print("Number of Non Tor Nodes : ",i)
+    print("Number of Non Tor Nodes : ", i)
 
     # print(soup.prettify()) # print and write to a file to vim-diff
     print()
@@ -41,17 +46,18 @@ def open_non_tor(url, webs,path):
     # pickle.dump( cook , open("cookies.pkl","wb"))
     print(cook)
     print()
-    txe = path+"/non_tor_["+webs.replace("/","")+"].txt"
+    txe = path+"/non_tor_["+webs.replace("/", "")+"].txt"
     f = open(txe, "w")
     f.write(soup.prettify())
 
     # ss_tor(browser,webs)
     # script(browser,url)
-    ss_non_tor(browser,webs,path)
+    ss_non_tor(browser, webs, path)
     close(browser)
     return cook
 
-def open_tor(url, webs, cook,path):
+
+def open_tor(url, webs, cook, path):
 
     ### OPEN USING TOR CONFIG ###
     print("Opening Tor...")
@@ -72,7 +78,7 @@ def open_tor(url, webs, cook,path):
     # browser.add_cookie(cook)
     # cookies = pickle.load(open("cookies.pkl", "rb"))
 
-    #Load Cookie from Non-Tor
+    # Load Cookie from Non-Tor
 
     # for cookie in cook:
     #     browser.add_cookie(cookie)
@@ -81,43 +87,42 @@ def open_tor(url, webs, cook,path):
     print()
     print("---------------HTML(TOR)-----------------")
     print()  # BLANK LINE
+    time.sleep(20)
     all_text = browser.page_source
     soup = BeautifulSoup(all_text, 'html.parser')
 
-    #BS4 prettify 
+    # BS4 prettify
 
-    print("Length   ",len(all_text)) # Length
+    print("Length   ", len(all_text))  # Length
     print("All Nodes (Non-Tor): ")
 
     # Number of nodes
-    i=0
+    i = 0
     for tag in soup.find_all(True):
-        i+=1
-        print(i," -- ", tag.name) # Print the names of DOM nodes
+        i += 1
+        print(i, " -- ", tag.name)  # Print the names of DOM nodes
     print()
-    print("Number of Tor Nodes : ",i)
+    print("Number of Tor Nodes : ", i)
 
     # print(soup.prettify()) # print and write to a file to vim-diff
     print()
     print("Cookies: ")
     print(browser.get_cookies())
     print()
-    
-    txe = path+"/tor_["+webs.replace("/","")+"].txt"
+
+    txe = path+"/tor_["+webs.replace("/", "")+"].txt"
     f = open(txe, "w")
     f.write(soup.prettify())
 
-    
-
-    
     # ss_tor(browser,webs)
     # script(browser,url)
-    ss_tor(browser,webs,path)
+    ss_tor(browser, webs, path)
     close(browser)
-    
+
     # print(browser.page_source)
 
-def script(browser,url):
+
+def script(browser, url):
     # # USE JS TO GET HTTP STATUS
     js = '''
     var xhr = new XMLHttpRequest();
@@ -133,15 +138,18 @@ def script(browser,url):
     print()
     # browser.save_screenshot("tor_"+url.replace("/","").replace(".","_")+".png")
 
-def ss_tor(browser, web,url):
-    time.sleep(5)
+
+def ss_tor(browser, web, url):
+    # time.sleep(10)
     print("Screenshoting tor...")
     browser.save_screenshot(url+"/tor_["+web.replace("/", "")+"].png")
 
-def ss_non_tor(browser,web,url):
-    time.sleep(5)
+
+def ss_non_tor(browser, web, url):
+    # time.sleep(10)
     print("Screenshoting non-tor...")
     browser.save_screenshot(url+"/non-tor_["+web.replace("/", "")+"].png")
+
 
 def close(browser):
     browser.close()
@@ -180,75 +188,82 @@ def close(browser):
 # browser.close()
 
 
-
 protocol = "https://"
 web_list = [
-    # "twitter.com"
-            # "dan.me.uk",
-            # "google.com",
-            # "Youtube.com",
-            # # "Tmall.com",
-            # # "Baidu.com",
-            # # "Qq.com",
-            # # "Sohu.com",
-            # "Facebook.com",
-            # # "Taobao.com",
-            # # "360.cn",
-            # # "Jd.com",
-            # "Amazon.com",
-            # "Yahoo.com",
-            # "Wikipedia.org",
-            # # "Zoom.us",
-            # # "Weibo.com",
-            # # "Sina.com.cn",
-            # # "Xinhuanet.com",
-            # "Live.com",
-            # "Reddit.com",
-            # "Netflix.com",
-            # "Microsoft.com",
-            # "Office.com",
-            # # "Panda.tv",
-            # # "Zhanqi.tv",
-            # "Instagram.com",
-            # # "Alipay.com",
-            # # "Csdn.net",
-            # # "Vk.com",
-            # # "Google.com.hk",
-            # # "Myshopify.com",
-            # # "Okezone.com",
-            # # "Microsoftonline.com",
+            "dan.me.uk",
+            "www.lloydsbank.com/",
+            "www.bitstamp.net/",
+            "www.sc.com/",
+            "rankexploits.com/",
+            "www.dominos.com/",
+            "www.palemoon.org/",
+            "adsabs.harvard.edu/",
+            "www.montypython.net/",
+            "www.go4go.net/go/",
+        #----------------------#    
+            "google.com",
+            "Youtube.com",
+            # "Tmall.com",
+            # "Baidu.com",
+            # "Qq.com",
+            # "Sohu.com",
+            "Facebook.com",
+            # "Taobao.com",
+            # "360.cn",
+            # "Jd.com",
+            "Amazon.com",
+            "Yahoo.com",
+            "Wikipedia.org",
+            # "Zoom.us",
+            # "Weibo.com",
+            # "Sina.com.cn",
+            # "Xinhuanet.com",
+            "Live.com",
+            "Reddit.com",
+            "Netflix.com",
+            "Microsoft.com",
+            "Office.com",
+            # "Panda.tv",
+            # "Zhanqi.tv",
+            "Instagram.com",
+            # "Alipay.com",
+            # "Csdn.net",
+            # "Vk.com",
+            # "Google.com.hk",
+            # "Myshopify.com",
+            # "Okezone.com",
+            # "Microsoftonline.com",
             "bing.com",
-            # # "Yahoo.co.jp",
-            # "Twitch.tv",
-            # # "Naver.com",
-            # "Ebay.com",
-            # # "Bongacams.com",
-            # "Adobe.com",
-            # "Twitter.com",
-            # # "Aliexpress.com",
-            # "Amazon.in",
-            # # "Huanqiu.com",
-            # "Stackoverflow.com",
-            # # "Tianya.cn",
-            # # "Yy.com",
-            # # "Amazon.co.jp",
-            # # "Aparat.com",
-            # # "Chaturbate.com",
-            # "Linkedin.com",
-            # "check.torproject.org"
+            # "Yahoo.co.jp",
+            "Twitch.tv",
+            # "Naver.com",
+            "Ebay.com",
+            # "Bongacams.com",
+            "Adobe.com",
+            "Twitter.com",
+            # "Aliexpress.com",
+            "Amazon.in",
+            # "Huanqiu.com",
+            "Stackoverflow.com",
+            # "Tianya.cn",
+            # "Yy.com",
+            # "Amazon.co.jp",
+            # "Aparat.com",
+            # "Chaturbate.com",
+            "Linkedin.com",
+            "check.torproject.org"
 ]
 
 for web in web_list:
     pth = (pathlib.Path().absolute())
-    path = os.path.join(pth, web.replace("/","").replace(".","_"))
-    print("PATH: ",path)
+    path = os.path.join(pth, web.replace("/", "").replace(".", "_"))
+    print("PATH: ", path)
     os.mkdir(path)
     print("Directory created")
     url = protocol + web
     # print(url," ",web)
-    cook = open_non_tor(url,web,path)
+    cook = open_non_tor(url, web, path)
     print()
-    open_tor(url,web,cook,path)
+    open_tor(url, web, cook, path)
     print()
     time.sleep(10)
-    
